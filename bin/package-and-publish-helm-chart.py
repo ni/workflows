b2aeac7a605dd77a957897bb72c07a8d94121fe5
Lee Nowotny <lee.nowotny@ni.com>
2022-08-01 09:26:11 -0500

    Fixed version comparison bug and other cleanup

--- bin/package-and-publish-helm-chart.py
+++ bin/package-and-publish-helm-chart.py
@@ -4,57 +4,54 @@ import requests
 import subprocess
 import sys
 
+from packaging.version import Version
 from shutil import copyfile
+
+
+def get_chart_attribute(chart_filepath, attribute_name, default_value):
+  with open(chart_filepath) as file:
+    for line in file:
+      if line.startswith(f'{attribute_name}:'):
+        return line.split(':')[1].strip()
+  return default_value
 
 
 def main():
   args = sys.argv[1:]
-  chart_path = args[0]
+  chart_directory = args[0]
   working_directory = args[1]
   build_number = args[2]
-  is_prerelease = args[3]
+  is_prerelease = args[3] == 'true'
   application_version = args[4]
 
-  chart_filename = chart_path + '/Chart.yaml'
+  chart_filepath = chart_directory + '/Chart.yaml'
 
   print(f'build_number = {build_number}')
   print(f'is_prerelease = {is_prerelease}')
   print(f'application_version = {application_version}')
 
   username = os.getenv('ARTIFACTORY_USER')
-  token = os.getenv('ARTIFACTORY_TOKEN')
+  token = os.getenv('ARTIFACTORY_PASSWORD')
 
-  # Parse chart name from the Chart.yaml
-  chart_name = None
-  with open(chart_filename) as file:
-    for line in file:
-      if line.startswith('name:'):
-        chart_name = line.split(':')[1].strip()
-        print(f'Chart name: {chart_name}')
-        break
+  # Parse chart name from the Chart.yaml file
+  chart_name = get_chart_attribute(chart_filepath, 'name', None)
+  print(f'Chart name: {chart_name}')
 
-  # Parse version from the Chart.yaml
-  chart_version = '0.0.0'
-  with open(chart_filename) as file:
-    for line in file:
-      if line.startswith('version:'):
-        chart_version = line.split(':')[1].strip()
-        print(f'Chart version: {chart_version}')
-        break
+  # Parse chart version from the Chart.yaml file
+  chart_version_string = get_chart_attribute(chart_filepath, 'version', '0.0.0')
+  print(f'Chart version: {chart_version_string}')
 
-  version_elements = chart_version.split('.')
-  version_major = int(version_elements[0])
-  version_minor = int(version_elements[1])
-  version_patch = int(version_elements[2])
+  chart_version = Version(chart_version_string)
+  version_major = chart_version.major
+  version_minor = chart_version.minor
 
   # Retrieve the list of published helm chart versions from Artifactory
-  auth = (username, token)
   response = requests.get(
     url=f'https://niartifacts.jfrog.io/artifactory/rnd-helm-ci/ni/systemlink/{chart_name}/{version_major}/{version_minor}',
-    auth=auth)
+    auth=(username, token))
 
   # Find the highest version of the chart on Artifactory
-  highest_version = [version_major, version_minor, version_patch]
+  highest_version = chart_version
   if response.status_code == 200:
     print('Found existing charts on the repo...')
     # Parse HTTP response
@@ -64,7 +61,7 @@ def main():
         end = line.find('</a>')
         package_name = line[start:end]
         print(f'Package name: {package_name}')
-         # We need to parse the following:
+        # We need to parse the following:
         #  chartname-0.1.27.tgz
         #  chartname-0.1.27.tgz.prov
         #  chartname-0.1.27-pre.20220629.4.tgz
@@ -72,15 +69,13 @@ def main():
         result = re.match(pattern, package_name)
         groups = result.groups()
 
+        # package_vesrion does not include prelease portion of version
         package_version = groups[1]
         print(f'Package version: {package_version}')
-        package_version_elements = package_version.split('.')
-        package_major = int(package_version_elements[0])
-        package_minor = int(package_version_elements[1])
-        package_patch = int(package_version_elements[2])
 
-        if ((package_major >= highest_version[0]) and (package_minor >= highest_version[1]) and (package_patch > highest_version[2])):
-          highest_version = [package_major, package_minor, package_patch]
+        current_version = Version(package_version)
+        if current_version > highest_version:
+          highest_version = current_version
   elif (response.status_code == 404):
     print('No charts for this major.minor version exist.  Using current chart version.')
   else:
@@ -89,21 +84,20 @@ def main():
 
   print(f'Highest version: {highest_version}')
 
-  # Increment the helm chart version
-  package_major_version = highest_version[0]
-  package_minor_version = highest_version[1]
-  package_patch_version = highest_version[2]
+  # Increment the Helm chart version for committed builds
+  package_major_version = highest_version.major
+  package_minor_version = highest_version.minor
 
-  if is_prerelease == 'true':
-    updated_version_string = f'{package_major_version}.{package_minor_version}.{package_patch_version}-pre.{build_number}'
+  if is_prerelease:
+    updated_version_string = f'{highest_version}-pre.{build_number}'
   else:
-    updated_version_string = f'{package_major_version}.{package_minor_version}.{package_patch_version + 1}'
+    updated_version_string = f'{package_major_version}.{package_minor_version}.{highest_version.micro + 1}'
 
   print(f'Updated version: {updated_version_string}')
-  
-  # Package the helm chart using the newly incremented version
+
+  # Package the Helm chart using the newly incremented version
   # It's important to format command as a string here, as this is required for using shell=True on Linux.
-  command = f'helm package {chart_path} --version {updated_version_string} --app-version {application_version} --destination {working_directory}'
+  command = f'helm package {chart_directory} --version {updated_version_string} --app-version {application_version} --destination {working_directory}'
   proc = subprocess.run(command, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   if proc.returncode != 0:
     print(f'process return code = {str(proc.returncode)}')
@@ -129,7 +123,7 @@ def main():
   # Publish the Helm chart package
   with open(helm_package_filepath, 'rb') as f:
     data = f.read()
-    auth = (username, token)
+    auth = (username, password)
     response = requests.put(
       url='https://niartifacts.jfrog.io/artifactory/rnd-helm-ci/ni/systemlink/{chart_name}/{package_major}/{package_minor}/{helm_package}'.format(
         chart_name=chart_name,
